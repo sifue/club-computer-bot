25e6e9502808f8a04adb3668f3e5760e9dd96f0f
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2018-03-16 23:26:47 +0900

    FIX: add prettier

--- scripts/chat.js
+++ scripts/chat.js
@@ -12,14 +12,14 @@ const atCoderSender = createTweetSender('#club_computer', 'atcoder');
 const atCoderCalSender = createTweetSender('#club_computer', 'AtcoderCalendar');
 // const sifueTestSender = createTweetSender('#sifue_bot_test', 'sifue_4466'); // sifue検証用
 
-module.exports = (robot) => {
+module.exports = robot => {
   // cron ジョブ 毎分
   cron.schedule('* * * * *', () => {
     cronJob(robot);
   });
 
   // 「ボット名 test」 で動作検証
-  robot.respond(/test/i, (msg) => {
+  robot.respond(/test/i, msg => {
     cronJob(robot);
   });
 
@@ -37,8 +37,8 @@ function random(size) {
 }
 
 function sendMassage(msg) {
-  let message = "";
-  sMsg.forEach((word) => {
+  let message = '';
+  sMsg.forEach(word => {
     message += word[random(word.length)];
   });
   msg.send(message);
@@ -46,26 +46,36 @@ function sendMassage(msg) {
 
 function createTweetSender(room, screenName) {
   const tweetIdSet = new Set();
-  return function (robot) {
-    const params = {screen_name: screenName};
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  return function(robot) {
+    const params = { screen_name: screenName };
+    client.get('statuses/user_timeline', params, function(
+      error,
+      tweets,
+      response
+    ) {
       if (!error) {
-        if(tweetIdSet.size === 0) { // 初回はキャッシュを貯めるだけ
-          tweets.forEach((tweet) => {
+        if (tweetIdSet.size === 0) {
+          // 初回はキャッシュを貯めるだけ
+          tweets.forEach(tweet => {
             tweetIdSet.add(tweet.id_str);
           });
-          console.log(`Tweet set was initialized. room: ${room} screenName: ${screenName}`);
+          console.log(
+            `Tweet set was initialized. room: ${room} screenName: ${screenName}`
+          );
         } else {
-          tweets.forEach((tweet) => {
-            if(!tweetIdSet.has(tweet.id_str)) {
-              const message = `https://twitter.com/${screenName}/status/${tweet.id_str}`;
-              robot.send({room: room}, message);
+          tweets.forEach(tweet => {
+            if (!tweetIdSet.has(tweet.id_str)) {
+              const message = `https://twitter.com/${screenName}/status/${
+                tweet.id_str
+              }`;
+              robot.send({ room: room }, message);
               tweetIdSet.add(tweet.id_str);
             }
           });
         }
 
-        if(tweetIdSet.size > 100000) { // メモリ溢れ対策
+        if (tweetIdSet.size > 100000) {
+          // メモリ溢れ対策
           tweetIdSet.clear();
         }
       } else {
@@ -76,75 +86,24 @@ function createTweetSender(room, screenName) {
 }
 
 const sMsg = [
+  ['初めまして', 'はじめまして', 'こんにちは', 'ようこそ'],
+  ['君が', 'あなたが'],
+  ['ね', 'だね', '？', 'だよね？'],
   [
-    "初めまして",
-    "はじめまして",
-    "こんにちは",
-    "ようこそ",
+    '何をしてるかを知りたい',
+    'とりあえず活動したい',
+    '何をしたら良いかわからない'
   ],
-  [
-    "君が",
-    "あなたが",
-  ],
-  [
-    "ね",
-    "だね",
-    "？",
-    "だよね？",
-  ],
-  [
-    "何をしてるかを知りたい",
-    "とりあえず活動したい",
-    "何をしたら良いかわからない",
-  ],
-  [
-    "？",
-    "の？",
-  ],
-  [
-    "それなら",
-    "それならまず",
-    "とりあえず",
-  ],
-  [
-    "ここ",
-    "これ",
-    "このサイト",
-  ],
-  [
-    "(https://sites.google.com/a/nnn.ed.jp/club_computer/)",
-  ],
-  [
-    "を読んだら",
-    "を読めば",
-    "を読むのが",
-    "を見れば",
-    "を見るのが",
-  ],
-  [
-    "いいと",
-  ],
-  [
-    "思う",
-    "思うな",
-  ],
-  [
-    "\nもし分からない事があったら",
-  ],
-  [
-    "@deletend",
-    "ここのチャンネル"
-  ],
-  [
-    "に",
-  ],
-  [
-    "聞けば",
-    "質問すれば",
-  ],
-  [
-    "すぐ解決してくれるよ",
-    "親切に答えてくれるよ",
-    "5分で答えてくれるよ",
-  ],
-]
+  ['？', 'の？'],
+  ['それなら', 'それならまず', 'とりあえず'],
+  ['ここ', 'これ', 'このサイト'],
+  ['(https://sites.google.com/a/nnn.ed.jp/club_computer/)'],
+  ['を読んだら', 'を読めば', 'を読むのが', 'を見れば', 'を見るのが'],
+  ['いいと'],
+  ['思う', '思うな'],
+  ['\nもし分からない事があったら'],
+  ['@deletend', 'ここのチャンネル'],
+  ['に'],
+  ['聞けば', '質問すれば'],
+  ['すぐ解決してくれるよ', '親切に答えてくれるよ', '5分で答えてくれるよ']
+];
